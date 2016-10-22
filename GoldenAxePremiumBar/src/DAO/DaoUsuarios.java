521678cff38927d1fc49058d2f3ce1e25ae48de5
jhteixeira <henri_teixeira@hotmail.com>
2016-10-22 14:28:36 -0200

    Criação DaoUsuarios e Entidade Usuarios

---
+++ GoldenAxePremiumBar/src/DAO/DaoUsuarios.java
@@ -0,0 +1,66 @@
+package DAO;
+
+import java.sql.PreparedStatement;
+
+import Entity.Usuarios;
+
+public class DaoUsuarios extends ConnectionDAO {
+
+	public void salvar(Usuarios usuarios) {
+
+		String sql = "INSERT INTO USUARIO(ID_FUNCIONARIO, NOME_USU, SENHA,) VALUES(?,?,?)";
+
+		try {
+			conectaBanco();
+			pst = conn.prepareStatement(sql);
+			pst.setInt(1, usuarios.getId_Funcionario());
+			pst.setString(2, usuarios.getUsuario());
+			pst.setString(3, usuarios.getSenha());
+			pst.execute();
+
+			pst.close();
+
+			desconectaBanco();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+	public void alterar(Usuarios usuarios) {
+
+		String sql = "UPDATE USUARIO SET ID_FUNCIONARIO = ?, NOME_USU = ?, SENHA = ? WHERE ID_USUARIO = ?";
+
+		try {
+			conectaBanco();
+			pst = conn.prepareStatement(sql);
+			pst.setInt(1, usuarios.getId_Funcionario());
+			pst.setString(2, usuarios.getUsuario());
+			pst.setString(3, usuarios.getSenha());
+			pst.execute();
+
+			pst.close();
+
+			desconectaBanco();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	public void deletar(Integer id_Usuario) {
+		String sql = "DELETE FROM USUARIO WHERE NOME_USU = ?";
+		try {
+			conectaBanco();
+			pst = conn.prepareStatement(sql);
+			pst.setInt(1, id_Usuario);
+			pst.execute();
+			pst.close();
+
+			desconectaBanco();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
