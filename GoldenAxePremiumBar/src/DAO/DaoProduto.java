22af70a8f7a54cb8237fadc22fe3628cac7f84ed
jhteixeira <henri_teixeira@hotmail.com>
2016-10-22 16:04:03 -0200

    Alteração DaoProdutos

--- GoldenAxePremiumBar/src/DAO/DaoProduto.java
+++ GoldenAxePremiumBar/src/DAO/DaoProduto.java
@@ -5,7 +5,7 @@ import Entity.Produto;
 public class DaoProduto extends ConnectionDAO{
 	public void salvar(Produto produto) {
 
-		String sql = "INSERT INTO PRODUTO(NOME, QUANTIDADE,VALOR, ID_FORNECEDOR) VALUES(?,?,?,?)";
+		String sql = "INSERT INTO PRODUTOS(NOME, QUANTIDADE,VALOR, ID_FORNECEDOR) VALUES(?,?,?,?)";
 
 		try {
 			conectaBanco();
@@ -26,16 +26,17 @@ public class DaoProduto extends ConnectionDAO{
 
 	}
 
-	public void alterar(Usuarios usuarios) {
+	public void alterar(Produto produto) {
 
-		String sql = "UPDATE USUARIO SET ID_FUNCIONARIO = ?, NOME_USU = ?, SENHA = ? WHERE ID_USUARIO = ?";
+		String sql = "UPDATE PRODUTOS SET NOME=?, QUANTIDADE=?,VALOR=?, ID_FORNECEDOR=? WHERE ID_PRODUTO=?";
 
 		try {
 			conectaBanco();
 			pst = conn.prepareStatement(sql);
-			pst.setInt(1, usuarios.getId_Funcionario());
-			pst.setString(2, usuarios.getUsuario());
-			pst.setString(3, usuarios.getSenha());
+			pst.setString(1, produto.getNome());
+			pst.setInt(2, produto.getQuantidade());
+			pst.setFloat(3, produto.getValor());
+			pst.setInt(4, produto.getIdFornecedor());
 			pst.execute();
 
 			pst.close();
@@ -47,12 +48,12 @@ public class DaoProduto extends ConnectionDAO{
 		}
 	}
 
-	public void deletar(Integer id_Usuario) {
-		String sql = "DELETE FROM USUARIO WHERE ID_USUARIO = ?";
+	public void deletar(Integer id_Produto) {
+		String sql = "DELETE FROM PRODUTOS WHERE ID_PRODUTO = ?";
 		try {
 			conectaBanco();
 			pst = conn.prepareStatement(sql);
-			pst.setInt(1, id_Usuario);
+			pst.setInt(1, id_Produto);
 			pst.execute();
 			pst.close();
 
